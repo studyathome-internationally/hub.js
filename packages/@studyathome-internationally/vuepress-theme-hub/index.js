424f3572cb0bc355118906ed594b3b607a740541
Alija Sabic <sabic@technikum-wien.at>
2019-08-22 16:36:14 +0200

    @studyathome-internationally | vuepress-theme-hub: add vuepress-plugin-hub and default options, change accent color

--- packages/@studyathome-internationally/vuepress-theme-hub/index.js
+++ packages/@studyathome-internationally/vuepress-theme-hub/index.js
@@ -1,3 +1,29 @@
-module.exports = {
-  extend: "@vuepress/theme-default"
+module.exports = (themeConfig, ctx) => {
+  const defaultHubPluginOptions = {
+    directories: [
+      {
+        id: "courses",
+        dirname: "_courses",
+        path: "/courses/",
+        layout: "IndexCourse",
+        itemLayout: "Course"
+      }
+    ]
+  };
+
+  const { modifyHubPluginOptions } = themeConfig;
+
+  const hubPluginOptions =
+    typeof modifyHubPluginOptions === "function"
+      ? modifyHubPluginOptions(defaultHubPluginOptions)
+      : defaultHubPluginOptions;
+
+  const plugins = ["@studyathome-internationally/hub", hubPluginOptions];
+
+  const config = {
+    extend: "@vuepress/theme-default",
+    plugins
+  };
+
+  return config;
 };
