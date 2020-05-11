295fdd4a4fbca63c98a85f1d1ce48d1623e88110
Alija Sabic <sabic@technikum-wien.at>
2020-05-11 15:46:03 +0200

    Update @studyathome-internationally/vuepress-theme-hub: remove test cases, update and refactor dependencies

--- packages/@studyathome-internationally/vuepress-theme-hub/__tests__/components/buttons/mailer.test.js
+++
@@ -1,9 +0,0 @@
-// import Vue from "vue";
-import Mailer from "../../../components/buttons/Mailer.vue";
-
-describe("Mailer", () => {
-  // Inspect the raw component options
-  it("has a property name", () => {
-    expect(typeof Mailer.name).toBe("string");
-  });
-});
