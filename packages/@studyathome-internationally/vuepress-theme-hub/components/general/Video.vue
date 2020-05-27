0f3f3ee042580a1c4f0c708a1087dacac5c81a99
Alija Sabic <sabic@technikum-wien.at>
2020-05-27 10:17:40 +0200

    Update @studyathome-internationally/vuepress-theme-hub: fix video styling in course page

--- packages/@studyathome-internationally/vuepress-theme-hub/components/general/Video.vue
+++ packages/@studyathome-internationally/vuepress-theme-hub/components/general/Video.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="container">
+    <!-- <div style="width: inherit; height: inherit;" class="player" :id="id"></div> -->
     <div class="player" :id="id"></div>
   </div>
 </template>
@@ -48,10 +49,4 @@ export default {
 };
 </script>
 
-<style lang="stylus" scoped>
-.container
-  position relative
-  width 100%
-  height 0
-  padding-bottom 56.27198%
-</style>
+<style lang="stylus" scoped></style>
