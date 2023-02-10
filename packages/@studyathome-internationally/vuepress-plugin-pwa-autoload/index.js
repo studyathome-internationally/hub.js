ade4c0ec6abbda8580459f7161b953a0c9f45315
Alija Sabic <sabic@technikum-wien.at>
2023-02-10 03:01:57 +0100

    Update @studyathome-internationally/vuepress-plugin-pwa-autoreload: correct globalUIComponents registration

--- packages/@studyathome-internationally/vuepress-plugin-pwa-autoload/index.js
+++ packages/@studyathome-internationally/vuepress-plugin-pwa-autoload/index.js
@@ -5,5 +5,5 @@ const { path } = require("@vuepress/shared-utils");
  */
 module.exports = {
   enhanceAppFiles: [path.resolve(__dirname, "enhanceAppFile.js")],
-  globalUIComponents: "AutoSWUpdatePopup.vue",
+  globalUIComponents: "AutoSWUpdatePopup",
 };
